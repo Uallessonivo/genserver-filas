55e97c6595be91a34bbbfdbecfca7e145b51c017
Uallesson Nunes Ivo <uallessons@gmail.com>
2022-05-29 11:56:41 -0300

    done

--- lib/genserver_queue.ex
+++ lib/genserver_queue.ex
@@ -1,18 +1,38 @@
 defmodule GenserverQueue do
-  @moduledoc """
-  Documentation for `GenserverQueue`.
-  """
+  use GenServer
 
-  @doc """
-  Hello world.
+  # CLient
+  def start_link(initial_stack) when is_list(initial_stack) do
+    GenServer.start_link(__MODULE__, initial_stack)
+  end
 
-  ## Examples
+  def enqueue(pid, stack) do
+    GenServer.cast(pid, {:enqueue, stack})
+  end
 
-      iex> GenserverQueue.hello()
-      :world
+  def dequeue(pid) do
+    GenServer.call(pid, :dequeue)
+  end
 
-  """
-  def hello do
-    :world
+  # Server
+  @impl true
+  def init(stack) do
+    {:ok, stack}
+  end
+
+  @impl true
+  def handle_cast({:enqueue, element}, queue) do
+    new_queue = queue ++ [element]
+    {:noreply, new_queue}
+  end
+
+  @impl true
+  def handle_call(:dequeue, _from, []) do
+    {:reply, :empty_queue, []}
+  end
+
+  @impl true
+  def handle_call(:dequeue, _from, [element | new_queue]) do
+    {:reply, element, new_queue}
   end
 end
