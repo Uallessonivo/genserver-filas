849225987f9af25f0d3531e4f5063eca576fdca6
Uallesson Nunes Ivo <uallessons@gmail.com>
2022-05-29 11:33:49 -0300

    initial struct

---
+++ lib/genserver_queue.ex
@@ -0,0 +1,18 @@
+defmodule GenserverQueue do
+  @moduledoc """
+  Documentation for `GenserverQueue`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GenserverQueue.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
